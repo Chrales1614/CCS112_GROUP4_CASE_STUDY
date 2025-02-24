c9ba283d4bc01ba3d441a3fd506b4b1a24a1a755
Princes Duran <147385394+PrincesDuran@users.noreply.github.com>
2025-02-25 00:13:44 +0800

    Deleting patient information

---
+++ delete_patient.php
@@ -0,0 +1,14 @@
+<?php
+require 'db.php';
+
+if (isset($_GET['id'])) {
+    $id = $_GET['id'];
+
+    $stmt = $pdo->prepare("DELETE FROM patients WHERE id = ?");
+    if ($stmt->execute([$id])) {
+        echo "success";
+    } else {
+        echo "error";
+    }
+}
+?>
