e2678f9f0c7a0affff6d4efce7cba6d42b963943
Delgado_C <delgadocharles1614@gmail.com>
2025-02-25 23:28:47 +0800

    configuration

--- delete_patient.php
+++ delete_patient.php
@@ -3,7 +3,7 @@ require 'db.php';
 if (isset($_GET['id'])) {
     $id = $_GET['id'];
 
-    $stmt = $pdo->prepare("DELETE FROM patient WHERE id = ?");
+    $stmt = $pdo->prepare("DELETE FROM patients WHERE id = ?");
     if ($stmt->execute([$id])) {
         echo "success";
     } else {
